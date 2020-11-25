859179bc1d006e9a3af8c3c613ecfdbd6b3b2a2c
afifhusain <afifhusain2014@gmail.com>
2020-11-25 10:36:14 +0700

    validasi activity register

--- app/src/main/java/com/example/design1/RegisterActivity.java
+++ app/src/main/java/com/example/design1/RegisterActivity.java
@@ -3,14 +3,129 @@ package com.example.design1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.view.WindowManager;
+import android.widget.EditText;
+
+import com.google.android.material.textfield.TextInputLayout;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    TextInputLayout regName, regUsername, regEmail, regPhoneNo, regPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        regName = findViewById(R.id.name);
+        regUsername = findViewById(R.id.username);
+        regEmail = findViewById(R.id.email);
+        regPhoneNo = findViewById(R.id.phoneNo);
+        regPassword = findViewById(R.id.password);
+
+    }
+
+    private Boolean validateName() {
+        String val = regName.getEditText().getText().toString();
+        if (val.isEmpty()) {
+            regName.setError("Form nama harus diisi");
+            return false;
+        } else {
+            regName.setError(null);
+            regName.setErrorEnabled(false);
+            return  true;
+        }
+    }
+
+    private Boolean validateUsername() {
+        String val = regUsername.getEditText().getText().toString();
+        String noWhiteSpace = "\\A\\w{4,20}\\z";
+
+        if (val.isEmpty()) {
+            regUsername.setError("Form username harus diisi");
+            return false;
+        } else if (val.length()>=15) {
+            regUsername.setError("Username terlalu panjang");
+            return  false;
+        } else if (!val.matches(noWhiteSpace)) {
+            regUsername.setError("White space are not allowed");
+            return false;
+        }
+        else {
+            regUsername.setError(null);
+            regUsername.setErrorEnabled(false);
+            return  true;
+        }
+    }
+
+    private Boolean validateEmail() {
+        String val = regEmail.getEditText().getText().toString();
+        String emailPattern = "[a-zA-z0-9._-]+@[a-z]+\\.+[a-z]+";
+
+        if (val.isEmpty()) {
+            regEmail.setError("Form nama harus diisi");
+            return false;
+        } else if (!val.matches(emailPattern)) {
+            regEmail.setError("Invalid email address");
+            return false;
+        }
+        else {
+            regEmail.setError(null);
+            return  true;
+        }
+    }
+
+    private Boolean validatePhoneNo() {
+        String val = regPhoneNo.getEditText().getText().toString();
+
+        if (val.isEmpty()) {
+            regPhoneNo.setError("Form nama harus diisi");
+            return false;
+        } else {
+            regPhoneNo.setError(null);
+            return  true;
+        }
+    }
+
+    private Boolean validatePassword() {
+        String val = regPassword.getEditText().getText().toString();
+        String passwordVal = "^" +
+                //"(?=.*[0-9])" +         //at least 1 digit
+                //"(?=.*[a-z])" +         //at least 1 lower case letter
+                //"(?=.*[A-Z])" +         //at least 1 upper case letter
+                "(?=.*[a-zA-Z])" +      //any letter
+                "(?=.*[@#$%^&+=])" +    //at least 1 special character
+                "(?=\\s+$)" +           //no white spaces
+                ".{4,}" +               //at least 4 characters
+                "$";
+
+        if (val.isEmpty()) {
+            regPassword.setError("Form nama harus diisi");
+            return false;
+        } else if (!val.matches(passwordVal)) {
+            regPassword.setError("Invalid password");
+            return false;
+        } else {
+            regPassword.setError(null);
+            return  true;
+        }
+    }
+
+
+    public void registerUser(View view) {
+
+        if (!validateName() | !validatePassword() | !validatePhoneNo() | !validateEmail() | !validateUsername()) {
+            return;
+        }
+
+        String name = regName.getEditText().getText().toString();
+        String username = regUsername.getEditText().getText().toString();
+        String email = regEmail.getEditText().getText().toString();
+        String phonoNo = regPhoneNo.getEditText().getText().toString();
+        String password = regPassword.getEditText().getText().toString();
+
+
+
     }
 }
